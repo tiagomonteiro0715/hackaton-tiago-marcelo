c786c7889b6376356fc8d2f30700b5be68c10d2f
Tiago monteiro <tiago.monteiro0715@gmail.com>
2023-03-10 20:58:05 +0000

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -3,117 +3,162 @@ import 'package:dart_openai/openai.dart';
 
 void main() => runApp(const MyApp());
 
-/// `MyApp` is a `StatelessWidget` that returns a `MaterialApp` with a `title` and a `home` property
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   static const String _title = 'Flutter Code Sample';
 
-  /// `build` is a function that returns a widget
-  ///
-  /// Args:
-  ///   context (BuildContext): The current configuration.
-  ///
-  /// Returns:
-  ///   A MaterialApp widget.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: _title,
-      home: chatGPTAnswer(),
+      home: ChatGPTAnswer(),
     );
   }
 }
 
-/// `chatGPTAnswer` is a `StatefulWidget` that creates a `_chatGPTAnswerState` when it's
-/// `createState` method is called
-class chatGPTAnswer extends StatefulWidget {
-  const chatGPTAnswer({super.key});
+class ChatGPTAnswer extends StatefulWidget {
+  const ChatGPTAnswer({Key? key}) : super(key: key);
 
   @override
-  State<chatGPTAnswer> createState() => _chatGPTAnswerState();
+  State<ChatGPTAnswer> createState() => _ChatGPTAnswerState();
 }
 
-class _chatGPTAnswerState extends State<chatGPTAnswer> {
-
+class _ChatGPTAnswerState extends State<ChatGPTAnswer> {
   late TextEditingController _controller;
+  String _prompt = '';
+
+  /// `initState()` is a function that is called when the widget is first created.
+  ///
+  /// `super.initState()` is a function that is called when the widget is first created.
+  ///
+  /// `_controller` is a variable that is called when the widget is first created.
+  ///
+  /// `TextEditingController()` is a function that is called when the widget is first created.
   @override
   void initState() {
     super.initState();
     _controller = TextEditingController();
   }
-  /// _generateTextFromChatGPTAPI() is a function that takes in a string and returns a string. It uses the
-  /// OpenAI API to generate text based on the input string._
+
+  /// It takes a string as input, and returns a string as output
+  ///
+  /// Args:
+  ///   gptPrompt (String): This is the text that you want to use to generate the response.
   ///
   /// Returns:
   ///   A string of text.
-  Future<String> _generateTextFromChatGPTAPI() async {
-    /// It's setting the API key for the OpenAI API.
-    OpenAI.apiKey = "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";
 
-    /// It's calling the OpenAI API to generate text based on the input string.
+  Future<String> _generateTextFromChatGPTAPI(String gptPrompt) async {
+    OpenAI.apiKey = "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";
+
     final completion = await OpenAI.instance.completion.create(
       model: "text-davinci-003",
-      prompt: "What is the meaning of life?",
-      maxTokens: 5,
+      prompt: gptPrompt,
+      maxTokens: 50,
     );
 
     return (completion.choices[0].text);
   }
 
+  /// It takes a string as input, and returns a string as output
+  ///
+  /// Args:
+  ///   context (BuildContext): The context of the widget.
+  ///
+  /// Returns:
+  ///   A string.
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: Theme.of(context).textTheme.displayMedium!,
-      textAlign: TextAlign.center,
-      child: FutureBuilder<String>(
-        future:
-            _generateTextFromChatGPTAPI(), // a previously-obtained Future<String> or null
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          /// It's checking if the snapshot has data. If it does, it's setting the children to a list of widgets.
-          List<Widget> children;
-          if (snapshot.hasData) {
-            children = <Widget>[
-              /// It's adding padding to the text.
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 16, bottom: 16, left: 16, right: 16),
-                child: Text(
-                  'You might have: ${snapshot.data}',
-                  textScaleFactor: 0.5,
-                ),
+    return Scaffold(
+      /// Creating the app bar.
+      appBar: AppBar(
+        title: const Text('Chat with GPT'),
+      ),
+
+      body: Column(
+        children: [
+          /// Creating a text field.
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: 'Enter your message',
+                border: OutlineInputBorder(),
               ),
-            ];
-          } else if (snapshot.hasError) {
-            children = <Widget>[
-              const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Error: ${snapshot.error}'),
-              ),
-            ];
-          } else {
-            children = const <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text(
-                  'Awaiting result...',
-                  textScaleFactor: 0.5,
-                ),
-              ),
-            ];
-          }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: children,
+
+              /// Setting the value of the text field to the value of the string.
+              onSubmitted: (String value) {
+                setState(() {
+                  _prompt = value;
+                });
+              },
             ),
-          );
-        },
+          ),
+
+          Expanded(
+            child: FutureBuilder<String>(
+              future: _generateTextFromChatGPTAPI(_prompt),
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                List<Widget> children;
+
+                /// Checking if the snapshot has data. If it does, it will return the data.
+                if (snapshot.hasData) {
+                  children = <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        '${snapshot.data}',
+                        style: const TextStyle(fontSize: 24.0),
+                      ),
+                    ),
+                  ];
+                }
+
+                /// If the data is loading, show a progress indicator. If the data is done loading,
+                /// show the data. If the data failed to load, show an error message
+                ///
+                /// Args:
+                ///    (snapshot):
+                else if (snapshot.hasError) {
+                  children = <Widget>[
+                    const Icon(
+                      Icons.error_outline,
+                      color: Colors.red,
+                      size: 60,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Text('Error: ${snapshot.error}'),
+                    ),
+                  ];
+                }
+
+                /// Showing the text "Type a message to start chatting" if the data is not loading.
+                else {
+                  children = const <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        'Type a message to start chatting',
+                        style: TextStyle(fontSize: 24.0),
+                      ),
+                    ),
+                  ];
+                }
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: children,
+                  ),
+                );
+              },
+            ),
+
+            /// Returning the data.
+          ),
+        ],
       ),
     );
   }
