4d6cdda1dc21910a8ebce66e9e4083c781629510
Tiago monteiro <tiago.monteiro0715@gmail.com>
2023-03-10 22:16:34 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,124 +4,80 @@ import 'package:dart_openai/openai.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: _title,
-      home: ChatGPTAnswer(),
+      home: chatGPTAnswer(),
     );
   }
 }
 
-class ChatGPTAnswer extends StatefulWidget {
-  const ChatGPTAnswer({Key? key}) : super(key: key);
+class chatGPTAnswer extends StatefulWidget {
+  const chatGPTAnswer({super.key});
 
   @override
-  State<ChatGPTAnswer> createState() => _ChatGPTAnswerState();
+  State<chatGPTAnswer> createState() => _chatGPTAnswerState();
 }
 
-class _ChatGPTAnswerState extends State<ChatGPTAnswer> {
+class _chatGPTAnswerState extends State<chatGPTAnswer> {
   late TextEditingController _controller;
-  String _prompt = '';
 
-  /// `initState()` is a function that is called when the widget is first created.
-  ///
-  /// `super.initState()` is a function that is called when the widget is first created.
-  ///
-  /// `_controller` is a variable that is called when the widget is first created.
-  ///
-  /// `TextEditingController()` is a function that is called when the widget is first created.
   @override
   void initState() {
     super.initState();
     _controller = TextEditingController();
   }
 
-  /// It takes a string as input, and returns a string as output
-  ///
-  /// Args:
-  ///   gptPrompt (String): This is the text that you want to use to generate the response.
-  ///
-  /// Returns:
-  ///   A string of text.
-
-  Future<String> _generateTextFromChatGPTAPI(String gptPrompt) async {
-    OpenAI.apiKey = "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";
-
+  Future<String> _generateTextFromChatGPTAPI(String APIprompt) async {
+    /// It's setting the API key for the OpenAI API.
+    OpenAI.apiKey = "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";
+    /// It's calling the OpenAI API to generate text based on the input string.
     final completion = await OpenAI.instance.completion.create(
       model: "text-davinci-003",
-      prompt: gptPrompt,
-      maxTokens: 50,
+      prompt: APIprompt,
+      maxTokens: 10,
     );
-
     return (completion.choices[0].text);
   }
 
-  /// It takes a string as input, and returns a string as output
-  ///
-  /// Args:
-  ///   context (BuildContext): The context of the widget.
-  ///
-  /// Returns:
-  ///   A string.
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    String userInput;
     return Scaffold(
-      /// Creating the app bar.
-      appBar: AppBar(
-        title: const Text('Chat with GPT'),
-      ),
-
-      body: Column(
-        children: [
-          /// Creating a text field.
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                hintText: 'Enter your message',
-                border: OutlineInputBorder(),
-              ),
-
-              /// Setting the value of the text field to the value of the string.
-              onSubmitted: (String value) {
-                setState(() {
-                  _prompt = value;
-                });
-              },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "DoctorAI",
+              textScaleFactor: 2,
             ),
-          ),
-
-          Expanded(
-            child: FutureBuilder<String>(
-              future: _generateTextFromChatGPTAPI(_prompt),
+            FutureBuilder<String>(
+              future: _generateTextFromChatGPTAPI(
+                  userInput), // a previously-obtained Future<String> or null
               builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
                 List<Widget> children;
-
-                /// Checking if the snapshot has data. If it does, it will return the data.
                 if (snapshot.hasData) {
                   children = <Widget>[
                     Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(
-                        '${snapshot.data}',
-                        style: const TextStyle(fontSize: 24.0),
-                      ),
+                      padding: const EdgeInsets.only(
+                          top: 16, bottom: 16, left: 16, right: 16),
+                      child: Text('Result: ${snapshot.data}'),
                     ),
                   ];
-                }
-
-                /// If the data is loading, show a progress indicator. If the data is done loading,
-                /// show the data. If the data failed to load, show an error message
-                ///
-                /// Args:
-                ///    (snapshot):
-                else if (snapshot.hasError) {
+                } else if (snapshot.hasError) {
                   children = <Widget>[
                     const Icon(
                       Icons.error_outline,
@@ -133,16 +89,18 @@ class _ChatGPTAnswerState extends State<ChatGPTAnswer> {
                       child: Text('Error: ${snapshot.error}'),
                     ),
                   ];
-                }
-
-                /// Showing the text "Type a message to start chatting" if the data is not loading.
-                else {
+                } else {
                   children = const <Widget>[
+                    SizedBox(
+                      width: 30,
+                      height: 30,
+                      child: CircularProgressIndicator(),
+                    ),
                     Padding(
-                      padding: EdgeInsets.all(16.0),
+                      padding: EdgeInsets.only(top: 16),
                       child: Text(
-                        'Type a message to start chatting',
-                        style: TextStyle(fontSize: 24.0),
+                        'Awaiting result...',
+                        textScaleFactor: 0.5,
                       ),
                     ),
                   ];
@@ -155,10 +113,19 @@ class _ChatGPTAnswerState extends State<ChatGPTAnswer> {
                 );
               },
             ),
-
-            /// Returning the data.
-          ),
-        ],
+            TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'What are you feeling?',
+              ),
+              controller: _controller,
+              onSubmitted: (String value) async {
+                userInput = value;
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
